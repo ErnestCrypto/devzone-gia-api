da49be4cf2c5b43aa1d25a6701529a47985665af
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-28 20:45:24 +0000

    adding allowed host

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', ]
 
 
 # Application definition
