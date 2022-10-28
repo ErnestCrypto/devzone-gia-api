a812a9c39e5a385dc926a10740ae024c44dab018
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-28 20:47:42 +0000

    adding endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-"""api URL Configuration
+"""GIAProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('giaAPI/', include('giaApp.urls', namespace="giaAppUrls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
