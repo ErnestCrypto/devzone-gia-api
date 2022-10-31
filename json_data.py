aed809335e965b15f2d21f6025f59e00996510f7
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-31 14:01:42 +0000

    joson_data file

--- json_data.py
+++ json_data.py
@@ -9,3 +9,31 @@
     "type": "business",
     "memberId": "GIA-93140087056380906936"
 }
+
+
+# creating a user account
+{
+    "firstname": "Kelvin",
+    "lastname": "Akoto Bamfo",
+    "dob": "12th september",
+    "gender": "male",
+    "email": "ernest6175@gmail.com",
+    "pin": "123412",
+    "phoneNumber": "0255421892",
+    "username": "crypto",
+    "memberType": "student",
+    "profileImage": "url",
+    "documents":
+    {
+                "ghanaCardNumber": "GHA-1938391892381921-0",
+                "frontCardPic": "kelvin",
+                "backCardPic": "kelvin"
+    }
+
+}
+
+
+# delete a request
+{
+    "requestId": "41ba8f80-bb97-4c4f-b493-0247f95b7cae"
+}
