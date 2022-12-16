1acd6ddc601d467035c062e23ab1563a906eaacb
MyFuelSmart <myfuelsmart@gmail.com>
2022-12-16 14:52:17 +0000

    commit

--- api/task.py
+++ api/task.py
@@ -8,7 +8,16 @@ from datetime import datetime, timedelta
 
 @shared_task(run_every=crontab(seconds=5))
 def deleteAccount():
-    
+    today = datetime.now()
+    deleted_users = Users.objects.filter(isDeleted=True)
+    try:
+        for x in deleted_users:
+            if today > x.expireDate:
+                x.delete()
+            else:
+                pass
+    except:
+        pass
     pass
   
   
